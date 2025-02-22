9acc9865dcf8b534f9ad318c867bf4fc2e5fa82e
Francesc Rocher <francesc.rocher@gmail.com>
2025-02-22 22:57:13 +0100

    Fix scripts

--- bin/create-all.sh
+++ bin/create-all.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
+source common
+
 # Pull all required docker images
+say get image NodeRED
 docker pull nodered/node-red
+
+say get image Eclipse-Mosquitto
 docker pull eclipse-mosquitto
+
+say get image InfluxDB
 docker pull influxdb
+
+say get image Grafana
 docker pull grafana/grafana
 
 # Create iot.net network
+say create network iot.net
 docker network create \
        --driver bridge \
        --subnet 172.22.0.0/16 \
@@ -14,6 +24,7 @@ docker network create \
        iot.net
 
 # Run all required docker containers
+say run container nred
 docker run \
        --detach \
        --hostname nred \
@@ -25,6 +36,7 @@ docker run \
        --volume $HOME/iot-server/vol/nred/data:/data \
        nodered/node-red
 
+say run container mqtt
 docker run \
        --detach \
        --hostname mqtt \
@@ -32,11 +44,12 @@ docker run \
        --network iot.net \
        --ip 172.22.0.12 \
        --publish 1883:1883 \
-       --volume $HOME/vol/mqtt/config:/mosquitto/config \
-       --volume $HOME/vol/mqtt/data:/mosquitto/data \
-       --volume $HOME/mosquitto/log:/mosquitto/log \
+       --volume $HOME/iot-server/vol/mqtt/config:/mosquitto/config \
+       --volume $HOME/iot-server/vol/mqtt/data:/mosquitto/data \
+       --volume $HOME/iot-server/vol/mqtt/log:/mosquitto/log \
        eclipse-mosquitto
 
+say run container flux
 docker run \
        --detach \
        --hostname flux \
@@ -44,8 +57,8 @@ docker run \
        --network iot.net \
        --ip 172.22.0.13 \
        --publish 8086:8086 \
-       --volume $HOME/vol/flux/data:/var/lib/influxdb2 \
-       --volume $HOME/vol/flux/config:/etc/influxdb2 \
+       --volume $HOME/iot-server/vol/flux/data:/var/lib/influxdb2 \
+       --volume $HOME/iot-server/vol/flux/config:/etc/influxdb2 \
        --env DOCKER_INFLUXDB_INIT_MODE=setup \
        --env DOCKER_INFLUXDB_INIT_USERNAME=qwe \
        --env DOCKER_INFLUXDB_INIT_PASSWORD=qweqweqwe \
@@ -53,6 +66,7 @@ docker run \
        --env DOCKER_INFLUXDB_INIT_BUCKET=smx1 \
        influxdb
 
+say run container gfna
 docker run \
        --detach \
        --hostname gfna \
@@ -61,5 +75,8 @@ docker run \
        --network iot.net \
        --ip 172.22.0.14 \
        --publish 3000:3000 \
-       --volume $HOME/vol/gfna/data:/var/lib/grafana \
+       --volume $HOME/iot-server/vol/gfna/data:/var/lib/grafana \
        grafana/grafana
+
+say status
+docker ps -a
--- bin/remove-all.sh
+++ bin/remove-all.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+source common
 
+say stop containers
 docker stop nred mqtt flux gfna
+
+say remove containers
 docker rm   nred mqtt flux gfna
+
+say remove network iot.net
 docker network rm iot.net
