e607c15e87fb4753ae277e1b4751836e682b8d63
Francesc Rocher <francesc.rocher@gmail.com>
2025-02-23 01:31:59 +0100

    Typo

--- bin/remove-all.sh
+++ bin/remove-all.sh
@@ -5,7 +5,7 @@ say stop containers
 docker stop nred mqtt flux gfna
 
 say remove containers
-docker rm   nred mqtt flux gfna
+docker rm nred mqtt flux gfna
 
 say remove network iot.net
 docker network rm iot.net
